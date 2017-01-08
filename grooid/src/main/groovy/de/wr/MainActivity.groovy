94c28b721c0cd25501fa63a0ef8a639d066b380a
Wolfgang Reithmeier <w.reithmeier@gmail.com>
2017-01-08 19:30:59 +0100

    initial groovy commit

---
+++ grooid/src/main/groovy/de/wr/MainActivity.groovy
@@ -0,0 +1,54 @@
+package de.wr
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import groovy.transform.CompileStatic
+import com.arasthel.swissknife.annotations.OnUIThread
+
+@CompileStatic
+class MainActivity extends Activity {
+      
+    @Override
+    void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+
+    @Override
+    boolean onCreateOptionsMenu(Menu menu) {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    @Override
+    boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.itemId
+
+        if (id == R.id.action_about) {
+            showAbout()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    @OnUIThread
+    void showAbout() {
+        int test = "test".with {
+            println "first char ${it.charAt(0)}"
+            it.indexOf('''t''')
+        }
+        boolean check = ["test", "34535", "else"].collect { it + "-mapped" }
+
+        new AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_launcher)
+            .setTitle(R.string.app_name)
+            .setView(layoutInflater.inflate(R.layout.about, null, false))
+            .create()
+            .show()
+    }
+    
+}
